4e6cd549d40a158fa58202abc8877b2d5332344d
Ginder Singh <ginder@windscribe.com>
2023-05-26 04:59:39 -0400

    update build.gradle

--- build.sh
+++ build.sh
@@ -1,17 +1,8 @@
-echo 'Setting go bind path...'
 export PATH=$PATH:~/go/bin
-echo 'Clean up...'
 go mod tidy
 go install golang.org/x/mobile/cmd/gomobile@latest
 gomobile init
 mkdir build
 cd proxy
-echo 'Building...'
-ls
-echo "xxxxxxxxxxxxxxx"
 gomobile bind -o ../build/proxy.aar  -javapkg com.windscribe
-echo "xxxxxxxxxxxxxxx"
-ls ../build
-echo "xxxxxxxxxxxxxxx"
-ls ~/.m2/repository/com
-echo 'Build successful.'
+echo 'Build successful...'
