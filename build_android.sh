21feae36e2832b0ed3bd077dc8d5cc6156e86ad6
Ginder Singh <ginder@windscribe.com>
2023-10-10 15:18:00 -0400

    add stack protection android binary.

--- build_android.sh
+++ build_android.sh
@@ -4,5 +4,7 @@ go install golang.org/x/mobile/cmd/gomobile@latest
 gomobile init
 mkdir build
 cd proxy
+export CGO_ENABLED=1
+export CGO_CFLAGS="-fstack-protector-strong"
 gomobile bind -o ../build/proxy.aar  -javapkg com.windscribe -ldflags "-s -w"
 echo 'Build successful...'
