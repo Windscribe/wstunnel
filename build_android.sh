b5fbde35c198f9c3e26b6a9239e57534e6ac5859
Ginder Singh <ginder@windscribe.com>
2024-07-09 16:49:24 -0400

    update ndk path update.

--- build_android.sh
+++ build_android.sh
@@ -13,28 +13,28 @@ echo "$buildCommand"
 
 # For ARM64
 output_dir="./build/android/arm64-v8a"
-TOOLCHAIN=("$ANDROID_NDK/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android21-clang")
+TOOLCHAIN=("$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang")
 # shellcheck disable=SC2086
 GOOS=android GOARCH=arm64 CC="${TOOLCHAIN[0]}" output_dir="$output_dir" sh -c "$buildCommand"
 rm $output_dir/libproxy.h
 
 ## For ARMv7
 output_dir="./build/android/armeabi-v7a"
-TOOLCHAIN=("$ANDROID_NDK/toolchains/llvm/prebuilt/darwin-x86_64/bin/armv7a-linux-androideabi21-clang")
+TOOLCHAIN=("$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi21-clang")
 # shellcheck disable=SC2086
 GOOS=android GOARCH=arm CC="${TOOLCHAIN[0]}" output_dir="$output_dir" sh -c "$buildCommand"
 rm $output_dir/libproxy.h
 
 ## For x86
 output_dir="./build/android/x86"
-TOOLCHAIN=("$ANDROID_NDK/toolchains/llvm/prebuilt/darwin-x86_64/bin/i686-linux-android21-clang")
+TOOLCHAIN=("$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android21-clang")
 # shellcheck disable=SC2086
 GOOS=android GOARCH=386 CC="${TOOLCHAIN[0]}" output_dir="$output_dir" sh -c "$buildCommand"
 rm $output_dir/libproxy.h
 
 ## For x86_64
 output_dir="./build/android/x86_64"
-TOOLCHAIN=("$ANDROID_NDK/toolchains/llvm/prebuilt/darwin-x86_64/bin/x86_64-linux-android21-clang")
+TOOLCHAIN=("$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android21-clang")
 # shellcheck disable=SC2086
 GOOS=android GOARCH=amd64 CC="${TOOLCHAIN[0]}" output_dir="$output_dir" sh -c "$buildCommand"
 rm $output_dir/libproxy.h
