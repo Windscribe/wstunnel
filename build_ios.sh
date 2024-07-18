43ff1cc58714a1dff8d0e74dafefaf518bb4fc0f
Ginder Singh <ginder@windscribe.com>
2024-07-18 16:23:12 -0400

    build for ios and tvos

--- build_ios.sh
+++ build_ios.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
+
+# Goos for both tv and iphoneos is "ios"
 export GOOS=ios
 export GOARCH=arm64
 export CGO_ENABLED=1
-export SDK=iphoneos
-export CGO_CFLAGS="-fembed-bitcode"
-export MIN_VERSION=15
-. ./target.sh
-export CGO_LDFLAGS="-target ${TARGET} -syslibroot \"${SDK_PATH}\""
-CC="$(pwd)/clangwrap.sh"
+
+# Build for Apple TVOS
+export SDK=appletvos
+CC="clang -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/AppleTVOS.platform/Developer/SDKs/AppleTVOS17.5.sdk -mtvos-version-min=17.0 -fembed-bitcode"
+CGO_CFLAGS=""
+CGO_LDFLAGS="-framework CoreFoundation"
 export CC
-rm -r build/ios
-output_dir="./build/ios/arm64"
-mkdir -p build/ios/arm64
-go build -buildmode=c-archive -o $output_dir/wstunnel.a cli.go logger.go httpclient.go stunnelbidirection.go websocketbidirConnection.go common.go
+
+rm -rf build/"$SDK"
+output_dir="./build/"$SDK"/arm64"
+mkdir -p "$output_dir"
+go build -buildmode=c-archive -o "$output_dir/proxy_tv.a" cli.go logger.go httpclient.go stunnelbidirection.go websocketbidirConnection.go common.go
+
+# Build for iPhoneOS
+export SDK=iphoneos
+CC="clang -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS17.5.sdk -miphoneos-version-min=12.0 -fembed-bitcode"
+CGO_CFLAGS=""
+CGO_LDFLAGS="-framework CoreFoundation"
+export CC
+
+rm -rf build/"$SDK"
+output_dir="./build/"$SDK"/arm64"
+mkdir -p "$output_dir"
+go build -buildmode=c-archive -o "$output_dir/proxy.a" cli.go logger.go httpclient.go stunnelbidirection.go websocketbidirConnection.go common.go
+echo "Apple TVOS framework at $output_dir/proxy_tv.a"
+echo "iPhoneOS framework at $output_dir/proxy.a"
