feb25a6f838e56c7bfb34c8db9caad91f9e120fb
Ginder Singh <ginder@windscribe.com>
2024-08-06 17:52:23 -0400

    build combined .xcframework for all platforms.

--- build_ios.sh
+++ build_ios.sh
@@ -1,32 +1,56 @@
 #!/bin/sh
 
-# Goos for both tv and iphoneos is "ios"
+# Common variables
 export GOOS=ios
 export GOARCH=arm64
 export CGO_ENABLED=1
 
-# Build for Apple TVOS
-export SDK=appletvos
-CC="clang -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/AppleTVOS.platform/Developer/SDKs/AppleTVOS17.5.sdk -mtvos-version-min=17.0 -fembed-bitcode"
-CGO_CFLAGS=""
-CGO_LDFLAGS="-framework CoreFoundation"
-export CC
+build() {
+    local sdk=$1
+    local min_version=$2
+    local platform_sdk=$3
 
-rm -rf build/"$SDK"
-output_dir="./build/"$SDK"/arm64"
-mkdir -p "$output_dir"
-go build -buildmode=c-archive -o "$output_dir/proxy_tv.a" cli.go logger.go httpclient.go stunnelbidirection.go websocketbidirConnection.go common.go
+    export SDK=$sdk
+    CC="clang -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/${platform_sdk}.platform/Developer/SDKs/${platform_sdk}17.5.sdk -m${sdk}-version-min=${min_version} -fembed-bitcode"
+    CGO_CFLAGS=""
+    CGO_LDFLAGS="-framework CoreFoundation"
+    export CC
+
+    output_dir="./build/${sdk}/arm64"
+    rm -rf "$output_dir"
+    mkdir -p "$output_dir"
+    go build -buildmode=c-archive -o "$output_dir/proxy.a" cli.go logger.go httpclient.go stunnelbidirection.go websocketbidirConnection.go common.go
+}
+
+# Build for Apple TVOS
+build "appletvos" "17.0" "AppleTVOS"
+echo "Apple TVOS framework at ./build/appletvos/arm64/proxy.a"
 
 # Build for iPhoneOS
-export SDK=iphoneos
-CC="clang -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS17.5.sdk -miphoneos-version-min=12.0 -fembed-bitcode"
-CGO_CFLAGS=""
-CGO_LDFLAGS="-framework CoreFoundation"
-export CC
+build "iphoneos" "12.0" "iPhoneOS"
+echo "iPhoneOS framework at ./build/iphoneos/arm64/proxy.a"
 
-rm -rf build/"$SDK"
-output_dir="./build/"$SDK"/arm64"
-mkdir -p "$output_dir"
-go build -buildmode=c-archive -o "$output_dir/proxy.a" cli.go logger.go httpclient.go stunnelbidirection.go websocketbidirConnection.go common.go
-echo "Apple TVOS framework at $output_dir/proxy_tv.a"
-echo "iPhoneOS framework at $output_dir/proxy.a"
+# Build for iPhoneSimulator
+build "iphonesimulator" "12.0" "iPhoneSimulator"
+echo "iPhoneSimulator framework at ./build/iphonesimulator/arm64/proxy.a"
+
+# Create a combined headers directory
+combined_headers_dir="./build/combined_headers"
+rm -rf "$combined_headers_dir"
+mkdir -p "$combined_headers_dir"
+
+# Copy header files from all builds
+cp ./build/appletvos/arm64/*.h "$combined_headers_dir"
+# same for all platforms
+#cp ./build/iphoneos/arm64/*.h "$combined_headers_dir"
+#cp ./build/iphonesimulator/arm64/*.h "$combined_headers_dir"
+
+# Create .xcframework
+rm -rf ./build/Proxy.xcframework
+xcodebuild -create-xcframework \
+    -library ./build/appletvos/arm64/proxy.a -headers "$combined_headers_dir" \
+    -library ./build/iphoneos/arm64/proxy.a -headers "$combined_headers_dir" \
+    -library ./build/iphonesimulator/arm64/proxy.a -headers "$combined_headers_dir" \
+    -output ./build/Proxy.xcframework
+
+echo "Combined framework at ./build/Proxy.xcframework"
